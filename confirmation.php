c00dd407d89a86a254fa4ac1d1a6dc3fdc5c7b4f
gshashidhar125 <gshashidhar125@gmail.com>
2013-09-14 19:14:18 -0700

    confirm

--- confirmation.php
+++ confirmation.php
@@ -8,14 +8,16 @@
 		    $phno =  $_POST['phno'];
 		    $gender =  $_POST['gender'];
     		    $password =  $_POST['password'];
+		    $password = md5($password);
 
 		    $query = "INSERT INTO `user_master`(`name`, `email`, `contact_num`, `gender`, `password`) VALUES ('".
 			      $username."','".$email."',".$phno.",'".$gender."','".$password."')";
 		  
                     $result = mysql_query($query, $connect)
             				or die('Error executing the query' . mysql_error());
-
+		    echo "Sign Up Success.. you will be redirected to login page";		
 		?>
+		
 	
 	</body>
 </html>
